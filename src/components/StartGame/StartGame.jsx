cac6d1e2a4566637f7137d33736c38ef9b527b36
Drazic <cy4kqq@hotmail.com>
2024-01-31 17:31:32 -0300

    Creating and adding new logo game page starter

--- src/components/StartGame/StartGame.jsx
+++ src/components/StartGame/StartGame.jsx
@@ -1,9 +1,10 @@
 const StartGame = ({ startGame }) => {
     return (
-        <main className="w-screen h-screen flex justify-center justify-items-center flex-col backdrop-blur-sm bg-black/60
+        <main className="w-screen h-screen flex justify-center justify-items-center items-center flex-col backdrop-blur-sm bg-black/60
         ">
-            <h1 className="text-8xl text-white text-center font-bold drop-shadow-2xl">Secret Cars Game</h1>
-            <p className="text-3xl text-blue-300 text-center font-semibold drop-shadow-2xl">Clique no botão para iniciar o jogo</p>
+            {/* <h1 className="text-8xl text-white text-center font-bold drop-shadow-2xl">Secret <span>Cars</span></h1> */}
+            <img className="h-1/2 -mb-24" src="/SecretCarsLogo.png" alt="Logo Secret Car" />
+            <p className="text-2xl text-blue-300 text-center drop-shadow-2xl">Clique no botão para iniciar o jogo</p>
             <div className="flex justify-center">
                 <button onClick={startGame} className="p-3 mt-10 text-2xl text-white rounded-md bg-purple-500 hover:bg-purple-600 transition ease-in-out shadow-2xl">Começar o jogo</button>
             </div>
--- src/data/cars.js
+++ src/data/cars.js
@@ -2,5 +2,9 @@ export const carsList = {
     VOLKSWAGEN: ['GOL', 'PARATI', 'SAVEIRO', 'GOLF', 'QUANTUM', 'AMAROK'],
     NISSAN: ['SENTRA', 'FRONTIER', 'KICKS', 'VERSA', 'SKYLINE'],
     SUBARU: ['IMPREZA', 'LEGACY', 'FOREST', '22B', 'VIVIO'],
-    HONDA: ['CIVIC', 'ACCORD', 'FIT']
-}
+    HONDA: ['CIVIC', 'ACCORD', 'FIT', 'CRV', 'HRV'],
+    TOYOTA: ['COROLLA', 'CAMRY', 'RAV4', 'HILUX', 'LAND CRUISER'],
+    FORD: ['FIESTA', 'FOCUS', 'MUSTANG', 'ESCAPE', 'EXPEDITION'],
+    CHEVROLET: ['CRUZE', 'MALIBU', 'TRAX', 'SILVERADO', 'TAHOE'],
+    MAZDA: ['MAZDA3', 'MAZDA6', 'CX-5', 'MX-5', 'RX-8']
+};
