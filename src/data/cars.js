f67161765428bf87e970d56d177e723915be8f80
Drazic <cy4kqq@hotmail.com>
2024-02-01 17:04:52 -0300

    Adding cars in archive cars.js

--- src/data/cars.js
+++ src/data/cars.js
@@ -1,12 +1,12 @@
-export const carsList = {
-    VOLKSWAGEN: ['GOL', 'PARATI'],
-};
+// Later on i will add more cars
 
-// VOLKSWAGEN: ['GOL', 'PARATI', 'SAVEIRO', 'GOLF', 'QUANTUM', 'AMAROK'],
- // NISSAN: ['SENTRA', 'FRONTIER', 'KICKS', 'VERSA', 'SKYLINE'],
-// SUBARU: ['IMPREZA', 'LEGACY', 'FOREST', '22B', 'VIVIO'],
-// HONDA: ['CIVIC', 'ACCORD', 'FIT', 'CRV', 'HRV'],
-// TOYOTA: ['COROLLA', 'CAMRY', 'RAV4', 'HILUX', 'LAND CRUISER'],
-// FORD: ['FIESTA', 'FOCUS', 'MUSTANG', 'ESCAPE', 'EXPEDITION'],
-// CHEVROLET: ['CRUZE', 'MALIBU', 'TRAX', 'SILVERADO', 'TAHOE'],
-// MAZDA: ['MAZDA3', 'MAZDA6', 'CX-5', 'MX-5', 'RX-8']
+export const carsList = {
+    VOLKSWAGEN: ['GOL', 'PARATI', 'SAVEIRO', 'GOLF', 'QUANTUM', 'AMAROK'],
+    NISSAN: ['SENTRA', 'FRONTIER', 'KICKS', 'VERSA', 'SKYLINE'],
+    SUBARU: ['IMPREZA', 'LEGACY', 'FOREST', '22B', 'VIVIO'],
+    HONDA: ['CIVIC', 'ACCORD', 'FIT', 'CRV', 'HRV'],
+    TOYOTA: ['COROLLA', 'CAMRY', 'RAV4', 'HILUX', 'LAND CRUISER'],
+    FORD: ['FIESTA', 'FOCUS', 'MUSTANG', 'ESCAPE', 'EXPEDITION'],
+    CHEVROLET: ['CRUZE', 'MALIBU', 'TRAX', 'SILVERADO', 'TAHOE'],
+    MAZDA: ['MAZDA3', 'MAZDA6', 'CX-5', 'MX-5', 'RX-8']
+};
